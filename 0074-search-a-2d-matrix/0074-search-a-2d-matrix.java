693a258c9cf6e4cf31a89a1a6ac816203372f9a9
Hema1006 <133997288+Hema1006@users.noreply.github.com>
2025-06-04 21:28:05 +0530

    Time: 0 ms (100%), Space: 42 MB (81.72%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,26 +1,18 @@
-
 class Solution {
-    public boolean searchMatrix(int[][] a, int target) {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int left=0,right=m*n-1;
         int flag=0;
-        
-        for(int i=0;i<a.length;i++)
-        {
-            for(int j=0;j<a[0].length;j++)
-            {
-                if(a[i][j]==target)
-                {
-                    flag=1;
-                }  
-        }
-    }
-        if(flag==1)
-        {
-            return true;
-        }
-        else
-        {
-           return false;
-        }
-       
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            int midele=matrix[mid/n][mid%n];
+            if(midele==target){
+               return true;}
+            else if(midele<target){
+                left=mid+1;}
+            else{
+                right=mid-1;}}
+    return false;
     }
 }
