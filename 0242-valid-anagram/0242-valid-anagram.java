4c0880dc0468c47e711b9c163d8f192e854d808c
Hema1006 <133997288+Hema1006@users.noreply.github.com>
2025-06-05 15:50:13 +0530

    Time: 4 ms (78.12%), Space: 43 MB (89.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,8 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()){return false;}
-        char[] ss=s.toCharArray();
-        char[] tt=t.toCharArray(); Arrays.sort(ss); Arrays.sort(tt);
-        return Arrays.equals(ss,tt);
+        int[] al = new int[26];
+        for(int i=0;i<s.length();i++) {
+            al[s.charAt(i)-'a']++;
         }
+        for(int i=0;i<t.length();i++){
+             al[t.charAt(i)-'a']--;
+        }
+        for(int i:al) {
+            if(i!=0){
+                 return false;}}
+        return true;
+    }
 }
+
